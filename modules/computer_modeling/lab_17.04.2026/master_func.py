73c6de4e405a1ba6b1b5a6ff80ca45ca74393914
Danila <zhukov24072001@gmail.com>
2026-06-09 12:34:15 +0300

    Add some files

--- modules/computer_modeling/lab_17.04.2026/master_func.py
+++ modules/computer_modeling/lab_17.04.2026/master_func.py
@@ -16,6 +16,56 @@ def lagrannge_function(data_x, data_y, necessary_val):
         result += lagrange * data_y[master]
     return result
 
+def newton_function(data_x, data_y, necessary_val):
+    n = len(data_x)
+    h = round(data_x[1] - data_x[0], 10)
+
+    # строим таблицу конечных разностей
+    delta = [data_y.copy()]
+    for order in range(1, 4):  # разности 1, 2, 3 порядка
+        delta.append([])
+        for j in range(len(delta[order - 1]) - 1):
+            delta[order].append(round(delta[order - 1][j + 1] - delta[order - 1][j], 9))
+
+    # интерполирование вперёд (x близко к началу таблицы)
+    if necessary_val <= data_x[len(data_x) // 2]:
+        # ищем подходящий узел x0
+        x0 = data_x[0]
+        index = 0
+        for i in range(len(data_x) - 1):
+            if data_x[i] <= necessary_val < data_x[i + 1]:
+                x0 = data_x[i]
+                index = i
+                break
+
+        q = (necessary_val - x0) / h
+
+        # формула Ньютона вперёд
+        result = (delta[0][index] +
+                  q * delta[1][index] +
+                  (q * (q - 1) / 2) * delta[2][index] +
+                  (q * (q - 1) * (q - 2) / 6) * delta[3][index])
+
+    # интерполирование назад (x близко к концу таблицы)
+    else:
+        # ищем подходящий узел xn
+        xn = data_x[-1]
+        index = len(data_x) - 1
+        for i in range(len(data_x) - 1, 0, -1):
+            if data_x[i - 1] < necessary_val <= data_x[i]:
+                xn = data_x[i]
+                index = i
+                break
+
+        q = (necessary_val - xn) / h
+
+        # формула Ньютона назад
+        result = (delta[0][index] +
+                  q * delta[1][index - 1] +
+                  (q * (q + 1) / 2) * delta[2][index - 2] +
+                  (q * (q + 1) * (q + 2) / 6) * delta[3][index - 3])
+
+    return result
 
     
 
--- modules/computer_modeling/lab_17.04.2026/task_2.1.1.py
+++ modules/computer_modeling/lab_17.04.2026/task_2.1.1.py
@@ -1,6 +1,6 @@
 from master_func import lagrannge_function
 
-necessary_val = float(input("Введите число от 0.43 до 0.72: "))
+necessary_val = float(input("Введите число от 0.43 до 0.75: "))
 
 data_x = [0.43,    0.48,    0.55,    0.62,    0.70,    0.75]
 data_y = [1.63597, 1.73234, 1.87686, 2.03345, 2.28846, 2.35973]  
